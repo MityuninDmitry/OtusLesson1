3e7935d8e697f01593f3e974fceb047c13229285
Mityunin Dmitry <mity-ml@mail.ru>
2023-06-01 22:23:08 +0500

    Initial Commit

---
+++ OtusLesson1/OtusLesson1App.swift
@@ -0,0 +1,17 @@
+//
+//  OtusLesson1App.swift
+//  OtusLesson1
+//
+//  Created by Dmitry Mityunin on 6/1/23.
+//
+
+import SwiftUI
+
+@main
+struct OtusLesson1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
